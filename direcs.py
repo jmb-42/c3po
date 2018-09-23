f55d8110fb0c49493f43ee1e04f131a6932229ba
Justin Beaurone <justinbeaurone@gmail.com>
2018-09-22 21:13:05 -0700

    Create direcs.py

---
+++ direcs.py
@@ -0,0 +1,7 @@
+# Directories (modify for your own system)
+PATH        = '/Users/lacc/Documents/Justin/c3po/' # Home dir of project folder
+STAR_FILES  = PATH + 'liz stars/'               # Dir with star files
+KURUCZ      = PATH + 'kurucz/'                     # Dir with kurucz models
+NEXTGEN     = PATH + 'nextgen/'                    # Dir with nextgen models
+IMG_DIR     = PATH + 'Realistic SEDS/'             # Output dir for images
+INTERPS_DIR = PATH + 'Arrays/InterpGrainTemps/'    # Dir with temp arrays
